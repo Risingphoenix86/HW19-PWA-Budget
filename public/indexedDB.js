0f9d62c7b26c345530502bd31cc4d12d5b449d69
Kyle Lux <kyleslux86@gmail.com>
2022-01-11 01:46:00 -0500

    added use indexedDB function and method actions

--- public/indexedDB.js
+++ public/indexedDB.js
@@ -6,3 +6,45 @@ export function checkForIndexedDb() {
     return true;
 }
 
+export function useIndexedDB(databaseName, storeName, method, object) {
+    return new Promise((resolve, reject) => {
+        const request = window.indexedDB.open(databaseName,1);
+        let db,
+        trans,
+        store;
+
+        request.onupgradeneeded = function(event) {
+            const db = request.result;
+            db.createObjectStore(storeName, { keyPath: '_id' });
+        };
+
+        request.onerror = function(event) {
+            console.log('error');
+        };
+
+        request.onsuccess = function(event) {
+            db = request.result;
+            trans = db.transaction(storeName, 'readwrite');
+            store = trans.objectStore(StoreName);
+
+            db.onerror = function(event) {
+                console.log('error');
+            };
+
+            if (method === 'get') {
+                const all = store.getAll();
+                all.onsuccess = function() {
+                    resolve(all.result);
+                };
+            } else if (method === 'put') {
+                store.put(object);
+            } else if (method === 'delete') { 
+                store.delete(object.id);
+            };
+
+            trans.oncomplete = function() {
+                db.close();
+            };
+        };
+    });
+}
